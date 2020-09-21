71a1da93cf7a20a72915c308be86ba9e2bdc9670
Your_Name <joseph161216@gmail.com>
2020-09-20 22:03:18 -0700

    update README

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import "./index.css";
+// import "./styles/styles.css";
 import * as serviceWorker from "./serviceWorker";
 import App from './App';
 
